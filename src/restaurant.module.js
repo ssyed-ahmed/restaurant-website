500390728b61e8556ee20455933e4920bcccb869
Sumair <sumair@coursera.com>
2016-11-19 17:06:30 +1100

    adding files.

---
+++ src/restaurant.module.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('restaurant', ['public'])
+    .config(config);
+
+  config.$inject = ['$urlRouterProvider'];
+  function config($urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+  }
+})();
